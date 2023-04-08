dac0187e86dc00a21db42c09c012019d130779c2
Toshiaki Maki <tmaki@vmware.com>
2023-04-08 15:52:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Hello Python
+
+```
+pack build app --builder paketobuildpacks/builder:base
+```
+
+```
+docker run --rm app
+```
+
---
+++ app.py
@@ -0,0 +1,9 @@
+import requests
+
+def main():
+    response = requests.get('https://httpbin.org/get')
+    print(response.text)
+
+if __name__ == '__main__':
+    main()
+
